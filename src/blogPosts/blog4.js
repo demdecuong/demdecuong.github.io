9e1c3bc2a8ae79747b69bc182124bbdf20c81ff4
demdecuong <nguyenphucminh2804@gmail.com>
2025-05-10 01:45:15 +0700

    add blog 4

---
+++ src/blogPosts/blog4.js
@@ -0,0 +1,14 @@
+const blog4 = {
+  id: 4,
+  title: "My first bycicle - Wise tips for new riders",
+  date: "March 2025",
+  link: "/blog/4",
+  description: "Best practice for your first bike from a veteran rider.",
+  tags: ["Cycling", "Personal Sharing"],
+  popular: true,
+  content: `
+    <p>This is the full content of the blog </p>
+  `,
+};
+
+export default blog4;
